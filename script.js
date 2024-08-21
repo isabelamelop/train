a454faeb6900f7f8fb9fad6735497df6823e134d
isabelamelop <128876888+isabelamelop@users.noreply.github.com>
2024-08-21 18:42:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,13 +13,11 @@ function togglePlay() {
         audioElement.play().catch(error => {
             console.error('Erro ao tentar tocar o áudio:', error);
         });
-        playPauseButton.style.backgroundColor = 'red'; // Muda a cor do botão para vermelho quando tocando
-        playPauseButton.innerHTML = ''; // Remove o texto do botão
+        playPauseButton.textContent = 'Pausar'; // Atualiza o texto do botão
         messageDiv.style.display = 'block'; // Exibe a mensagem
         morImage.style.display = 'block'; // Exibe a imagem
     } else {
         audioElement.pause();
-        playPauseButton.style.backgroundColor = 'transparent'; // Muda a cor do botão para transparente quando pausado
-        playPauseButton.innerHTML = ''; // Remove o texto do botão
+        playPauseButton.textContent = 'Dormir'; // Atualiza o texto do botão
     }
 }
