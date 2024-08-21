00b07abaca389e434c9ffa08313e77df986e4565
isabelamelop <128876888+isabelamelop@users.noreply.github.com>
2024-08-21 19:30:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-const audioElement = document.getElementById('musica');
 const playPauseButton = document.getElementById('playPause');
 const messageDiv = document.getElementById('message');
 const morImage = document.getElementById('morImage');
-
-// Define a URL da música do Google Drive
-audioElement.src = 'https://drive.google.com/uc?export=download&id=16d6q4S1cXT7KNfehiZpErScmBh3h-Z56';
-audioElement.loop = true;
+let currentFrame = 0;
+const frames = [
+    document.getElementById('audioPlayer1'),
+    document.getElementById('audioPlayer2'),
+    document.getElementById('audioPlayer3'),
+    document.getElementById('audioPlayer4')
+];
 
 // Função para tocar/pausar a música
 function togglePlay() {
-    if (audioElement.paused) {
-        audioElement.play().catch(error => {
-            console.error('Erro ao tentar tocar o áudio:', error);
-        });
-        playPauseButton.textContent = 'Pausar';
-        messageDiv.style.display = 'block'; // Exibe a mensagem
-        morImage.style.display = 'block'; // Exibe a imagem
-    } else {
-        audioElement.pause();
-        playPauseButton.textContent = 'Dormir';
+    const frame = frames[currentFrame];
+
+    // Tenta tocar o áudio e exibe erros, se houver
+    frame.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+    
+    if (frame.style.display === 'none') {
+        frame.style.display = 'block';
     }
+
+    // Exibir a mensagem e a imagem
+    if (frame.contentWindow.document.readyState === 'complete') {
+        playPauseButton.textContent = 'Pause';
+        messageDiv.style.display = 'block';
+        morImage.style.display = 'block';
+    }
+    
+    // Avança para o próximo iframe quando o atual acabar
+    frame.onload = () => {
+        frame.contentWindow.addEventListener('message', event => {
+            if (event.data === 'ended') {
+                frame.style.display = 'none'; // Oculta o frame atual
+                currentFrame = (currentFrame + 1) % frames.length; // Avança para o próximo frame
+                frames[currentFrame].style.display = 'block'; // Mostra o próximo frame
+                frames[currentFrame].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+            }
+        });
+    };
 }
